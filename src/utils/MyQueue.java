62ad8433bfacdef179d322751aab80a43a902312
yzl <fjt086@gmail.com>
2022-01-24 12:28:11 -0800

    add MyQueue add MyStack add my-api.iml

--- src/utils/MyQueue.java
+++ src/utils/MyQueue.java
@@ -1,11 +1,82 @@
 package utils;
 
-/*****************************************************************
- * This class MyQueue implements a FIFO data structure using a
- * Singly Linked List (SLL).
- *****************************************************************/
+import single.SinglyLinkedList;
 
-public class MyQueue {
+import java.util.NoSuchElementException;
 
-    //TODO : Complete Body with Data Fields, Methods and Classes
+public class MyQueue<E> {
+    Node first;
+    Node last;
+    int size;
+
+    //构造函数
+    public MyQueue(){
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    static class Node<E> {
+        E data;
+        Node next;
+
+        //构造函数
+        public Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node(Node next, E data) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private void append(E item){
+        Node t = new Node(last.next,item);
+        //是否为第一个
+        if(size == 0){
+            first = t;
+        }
+        last = t;
+        size ++;
+    }
+
+    public boolean add(E item){
+        append(item);
+        return true;
+    }
+
+    private void detach(){
+        first = first.next;
+        if(size == 1){
+            last = first;
+        }
+        size --;
+    }
+
+    public E remove(){
+        E t = (E) first.data;
+        if(size == 0){
+            throw new NoSuchElementException();
+        }
+        detach();
+        return t;
+    }
+
+    public E peek(){
+        E t =(E) first.data;
+        return t;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }
+        return false;
+    }
 }
--- src/utils/MyStack.java
+++ src/utils/MyStack.java
@@ -1,11 +1,80 @@
 package utils;
 
-/*****************************************************************
- * This class MyStack implements a LIFO data structure using a
- * Singly Linked List (SLL).
- *****************************************************************/
+import java.util.EmptyStackException;
 
-public class MyStack {
+public class MyStack<E> {
+    Node first;
+    Node last;
+    int size;
 
-    //TODO : Complete Body with Data Fields, Methods and Classes
+    //构造函数
+    public MyStack() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    static class Node<E>{
+        E data;
+        Node next;
+
+        //构造函数
+        public Node(Node next, E data) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 方法实现
+     */
+    private void detach(){
+        //如果只有一个元素
+        if(size == 1){
+            first = null;
+        }
+        last = last.next;
+        size --;
+    }
+
+    public E push(E item){
+        Node t = new Node(last,item);
+        last = t;
+        if(size == 0){
+            first = t;
+        }
+        size++;
+        return item;
+    }
+
+    public E pop(){
+        //检查堆栈
+        if(size == 0){
+            throw new EmptyStackException();
+        }
+        E t = (E) last.data;
+        //移除并替换第一个元素
+        detach();
+        return t;
+    }
+
+    public E peek(){
+        //检查堆栈
+        if(size == 0){
+            throw new EmptyStackException();
+        }
+        return (E) last.data;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if(size() == 0){
+            return true;
+        }
+        return false;
+    }
+
 }
