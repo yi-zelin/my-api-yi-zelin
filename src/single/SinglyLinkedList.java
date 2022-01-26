f52cc985868c5221a5052f50450a67d379725d6d
yzl <fjt086@gmail.com>
2022-01-26 14:16:25 -0800

    add SinglyLinkedList add SinglyLinkedList

--- src/single/SinglyLinkedList.java
+++ src/single/SinglyLinkedList.java
@@ -1,7 +1,7 @@
 package single;
 
 public class SinglyLinkedList<T> {
-    private Node first;
+    private Node<T> first;
     int size;
     //构造函数
     public SinglyLinkedList (){
@@ -14,9 +14,9 @@ public class SinglyLinkedList<T> {
      * 定义Node class
      * @param <T>
      */
-    private class Node<T> {
+    private static class Node<T> {
         T data;
-        Node next;
+        Node<T> next;
 
         //构造函数
         public Node(T data) {
@@ -24,76 +24,83 @@ public class SinglyLinkedList<T> {
             this.next = null;
         }
 
-        public Node(Node next, T data) {
+        public Node(Node<T> next, T data) {
             this.data = data;
             this.next = next;
         }
     }
 
-    //返回索引处的地址
-    private Node node(int index) {
-        Node t = first;
-        for (int i = 0; i < index; i++) {
+    private void checkIndex(int index){
+        if(index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private Node<T> node(int index){
+        Node<T> t = first;
+        for (int i = 0; i < index; i++){
             t = t.next;
         }
         return t;
     }
 
-    //检查位置的有效性
-    private void checkIndex(int index){
-        if (index < 0 | index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-    }
-
-    //返回索引处data
-    public T get(int index) {
-        //检查有效性
+    public T get(int index){
         checkIndex(index);
-        //索引地址与值
         Node<T> address = node(index);
-        T data = address.data;
-        return data;
+        return address.data;
     }
 
-    //size
-    public int size() {
-        int i = 0;
-        for (Node t = first; t != null; t = t.next) {
-            i++;
+    private T detach(int index){
+        T deleted = get(index);
+        if(index == 0){
+            first = node(index).next;
+        }else{
+            node(index - 1).next = node(index).next;
         }
-        return i;
+        size --;
+        return deleted;
     }
 
-    //isEmpty
-    public boolean isEmpty() {
-        if (first == null) {
-            return true;
-        }
-        return false;
-    }
-
-    //toString
-    public String toString() {
-        if(isEmpty()){
-            return "[]";
-        }
-        String stringList = "[";
-        for (Node t = first; t != null; t = t.next) {
-            if (t.next == null) {
-                stringList += t.data + "]";
-                break;
-            }
-            stringList += t.data + ", ";
-        }
-        return stringList.toString();
-    }
-
-    //替换对应的node
-    public T set(int index, T item) {
-        //检查有效性
+    private void insertBefore(int index, T item){
         checkIndex(index);
+        Node<T> t = new Node<>(node(index),item);
+        if (index == 0){
+            first = t;
+        }else{
+            node(index - 1).next = t;
+        }
+        size ++;
+    }
 
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void append(T item){
+        Node<T> t = new Node<>(item);
+        if (isEmpty()){
+            first = t;
+        } else {
+            node(size - 1).next = t;
+        }
+        size ++;
+    }
+
+    public boolean add(T item){
+        append(item);
+        return true;
+    }
+
+    public void add(int index, T item){
+        insertBefore(index,item);
+    }
+
+    public T remove(int index){
+        return detach(index);
+    }
+
+    public T set(int index,T item){
+        checkIndex(index);
         Node<T> address = node(index);
         T replaced = address.data;
         address.data = item;
@@ -104,81 +111,22 @@ public class SinglyLinkedList<T> {
         return replaced;
     }
 
-    //末尾添加
-    public void append (T item){
-        Node t = new Node(item);
-        //如果是第一个node
-        if (isEmpty()){
-            first = t;
-        } else {
-            //遍历得到末尾地址, 并将末尾指向新node
-            node(size - 1).next = t;
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        if(isEmpty()){
+            return "[]";
         }
-        size ++;
-    }
-
-    //删除
-    public T remove(int index){
-        T deleted = get(index);
-        //headnode check
-        if(index == 0){
-            first = node(index).next;
-        }else {
-            //之前node.next指向之后node的地址
-            node(index - 1).next = node(index).next;
-        }
-        size --;
-        return deleted;
-    }
-
-    //分离指定的node, 返回该node的位置
-    private T detach(int index){
-        T t = get(index);
-        node(index-1).next = null;
-        size = size();
-        return t;
-    }
-
-    //末尾添加, 以及检测是否添加成功
-    public boolean add(T item){
-        //排除第一个添加的情况
-        if (isEmpty()){
-            append(item);
-            //检查是否添加成功 (核对地址)
-            if (first != node(0)){
-                return false;
+        StringBuilder stringList = new StringBuilder("[");
+        for (Node<T> t = first; t != null; t = t.next) {
+            if (t.next == null) {
+                stringList.append(t.data).append("]");
+                break;
             }
-            return true;
-        }else {
-            //用核对末尾node地址的方式来确定是否添加成功
-            Node preAddress = node(size - 2).next;
-            append(item);
-            Node afterAddress = node(size - 2).next;
-            if (preAddress != afterAddress) {
-                return true;
-            }
+            stringList.append(t.data).append(", ");
         }
-        return false;
+        return stringList.toString();
     }
-
-    //指定位置添加
-    public void add(int index, T item){
-        //被添加的node, 以及使他指向原来在该位置上的节点
-        Node t = new Node(item);
-        t.next = node(index);
-        //headnode check
-        if (index == 0){
-            first = t;
-        }else {
-            node(index - 1).next = t;
-        }
-        size ++;
-    }
-
-    //指定位置之前添加
-    private void insertBefore(int index, T item){
-        checkIndex(index);
-        add(index-1, item);
-    }
-
 }
--- src/tests/console/SinglyLinkedList.java
+++ src/tests/console/SinglyLinkedList.java
@@ -2,13 +2,13 @@ package tests.console;
 
 public class SinglyLinkedList {
     public static void main(String[] args) {
-        single.SinglyLinkedList a = new single.SinglyLinkedList();
+        single.SinglyLinkedList<Integer> a = new single.SinglyLinkedList<>();
         System.out.println("\n********** add,append,toString test **********");
-        System.out.println(a.toString());
-        a.add(100);
-        a.add(111);
-        a.add(222);
-        System.out.println(a.toString());
+        System.out.println(a);
+        System.out.println(a.add(100));
+        System.out.println(a.add(111));
+        System.out.println(a.add(222));
+        System.out.println(a);
 
         System.out.println("\n********** checkIndex, get test **********");
         System.out.println(a.get(0));
@@ -17,27 +17,27 @@ public class SinglyLinkedList {
 
         System.out.println("\n********** add,size,set,node test **********");
         a.add(0,333);
-        System.out.println("size = "+ a.size()+" "+a.toString());
-        a.add(4,444);
-        System.out.println("size = "+ a.size()+" "+a.toString());
+        System.out.println("size = "+ a.size()+" "+a);
+        a.add(3,444);
+        System.out.println("size = "+ a.size()+" "+a);
         System.out.println("set return "+a.set(3,555));
-        System.out.println(a.toString());
+        System.out.println(a);
 
         System.out.println("\n********** isEmpty,remove test **********");
-        single.SinglyLinkedList b = new single.SinglyLinkedList();
+        single.SinglyLinkedList<String> b = new single.SinglyLinkedList<>();
         System.out.println(b.isEmpty());
         System.out.println(a.isEmpty());
         System.out.println("remove return: "+a.remove(0));
-        System.out.println(a.toString());
+        System.out.println(a);
         System.out.println("remove return: "+a.remove(3));
-        System.out.println(a.toString());
+        System.out.println(a);
 
         System.out.println("\n********** String test **********");
         b.add("one");
         b.add("two");
         b.add("three");
-        System.out.println(b.toString());
+        System.out.println(b);
         b.remove(2);
-        System.out.println(b.toString());
+        System.out.println(b);
     }
 }
