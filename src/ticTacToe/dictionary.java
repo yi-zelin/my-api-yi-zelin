18af4de11b42274fa4638a57c2b5b7ab25a4b1af
yzl <fjt086@gmail.com>
2022-02-14 03:16:43 -0800

    add Changes

--- src/ticTacToe/dictionary.java
+++ src/ticTacToe/dictionary.java
@@ -1,6 +1,7 @@
 package ticTacToe;
 
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,34 +25,21 @@ public class dictionary extends Tic{
      */
     Map<Integer,Integer> diagonal = new HashMap<>();  //对角线
     Map<Integer,Integer> opposite = new HashMap<>();  //对边
-    Map<Integer,Integer> left = new HashMap<>();      //左
-    Map<Integer,Integer> right = new HashMap<>();     //右
-    Map<Integer,Integer> up = new HashMap<>();        //上
-    Map<Integer,Integer> down = new HashMap<>();      //下
-    Map<Integer,Integer> leftUp = new HashMap<>();    //左上
-    Map<Integer,Integer> leftDown = new HashMap<>();  //左下
-    Map<Integer,Integer> rightUp = new HashMap<>();   //右上
-    Map<Integer,Integer> rightDown = new HashMap<>(); //右下
-    Map<Integer,Integer> middleDiagonal1 = new HashMap<>(); //横着目对角上
-    Map<Integer,Integer> middleDiagonal2 = new HashMap<>();
-    Map<Integer,Integer> middleDiagonal3 = new HashMap<>(); //竖着目对角左
-    Map<Integer,Integer> middleDiagonal4 = new HashMap<>();
+    Map<Integer,int[]> together = new HashMap<>();  //相邻
+    Map<Integer,int[]> close = new HashMap<>();  //相邻
+    Map<Integer,int[]> far = new HashMap<>();  //相邻
 
     dictionary(){
         diagonal.put(1,9);diagonal.put(9,1);diagonal.put(3,7);diagonal.put(7,3);
         opposite.put(2,8);opposite.put(8,2);opposite.put(4,6);opposite.put(6,4);
-        left.put(2,1);left.put(3,2);left.put(5,4);left.put(6,5);left.put(8,7);left.put(9,8);
-        right.put(1,2);right.put(2,3);right.put(4,5);right.put(5,6);right.put(7,8);right.put(8,9);
-        up.put(4,1);up.put(5,2);up.put(6,3);up.put(7,4);up.put(8,5);up.put(9,6);
-        down.put(1,4);down.put(2,5);down.put(3,6);down.put(4,7);down.put(5,8);down.put(6,9);
-        leftUp.put(5,1);leftUp.put(6,2);leftUp.put(8,4);leftUp.put(9,5);
-        leftDown.put(2,4);leftDown.put(3,5);leftDown.put(5,7);leftDown.put(6,8);
-        rightUp.put(4,2);rightUp.put(5,3);rightUp.put(7,5);rightUp.put(8,6);
-        rightDown.put(1,5);rightDown.put(2,6);rightDown.put(4,8);rightDown.put(5,9);
-        middleDiagonal1.put(1,6);middleDiagonal1.put(6,1);middleDiagonal1.put(3,4);middleDiagonal1.put(4,3);
-        middleDiagonal2.put(4,9);middleDiagonal2.put(9,4);middleDiagonal2.put(6,7);middleDiagonal2.put(7,6);
-        middleDiagonal3.put(1,8);middleDiagonal3.put(8,1);middleDiagonal3.put(2,7);middleDiagonal3.put(7,2);
-        middleDiagonal4.put(2,9);middleDiagonal4.put(9,2);middleDiagonal4.put(3,8);middleDiagonal4.put(8,3);
+        together.put(1,new int[] {2,4});together.put(2,new int[] {1,3,5});together.put(3,new int[] {2,6});
+        together.put(4,new int[] {1,5,7});together.put(5,middle);together.put(6,new int[] {3,5,9});
+        together.put(7,new int[] {4,8});together.put(8,new int[] {5,7,9});together.put(9,new int[] {6,8});
+        close.put(1,central);close.put(2,new int[] {4,6});close.put(3,central);
+        close.put(4,new int[] {2,8});close.put(5,corner);close.put(6,new int[] {2,8});
+        close.put(7,central);close.put(8,new int[] {4,6});close.put(9,central);
+        far.put(1,new int[] {3,6,7,8});far.put(3,new int[] {1,4,8,9});
+        far.put(7,new int[] {1,2,6,9});far.put(9,new int[] {2,3,4,7});
 
 
         formulaNumber = 0;
