9929ca4844358009a5f3e3e8798aeaeaac393580
yzl <fjt086@gmail.com>
2022-03-06 12:22:11 -0800

    add all tag v0.1.0

--- src/tests/console/TravelDirectionTest.java
+++ src/tests/console/TravelDirectionTest.java
@@ -36,9 +36,32 @@ public class TravelDirectionTest {
         System.out.println();
     }
 
+    private static void goBack(int endX, int endY, int x, int y, String route) {
+
+        if (x == endX && y == endY) {
+            System.out.println(route);
+
+        }else if(x <= endX && y <= endY){
+            goBack(endX, endY,      x, y + 1, route + " S");
+            goBack(endX, endY,x + 1,       y, route + " W");
+            goBack(endX, endY,x + 1, y + 1, route + " SW");
+        }
+
+        //OTHERWISE : DO NOTHING
+    }
+
+    public static void goBack(int startX, int startY, int endX, int endY) {
+        goBack(startX, startY, endX, endY, "movesBack:");
+    }
+
     public static void main(String[] args) {
         intro();
+        System.out.println("Movement 1\n");
         goNorthEast(1,2, 0, 0);
+        System.out.println("\n\nMovement 2\n");
+        goNorthEast(1,2, 0, 0);
+        System.out.println();
+        goBack(1,2,0,0);
         System.out.println();
     }
 }
